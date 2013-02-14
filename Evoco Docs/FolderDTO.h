ea4f5c4753fa3a157d691085bbe7bb1c65438c74
Gabor Shaio <gabor@voyagerweb.ca>
2013-02-14 09:26:13 -0700

    added folder dto

---
+++ Evoco Docs/FolderDTO.h
@@ -0,0 +1,13 @@
+//
+//  FolderDTO.h
+//  fuze Docs
+//
+//  Created by Gabor Shaio on 2013-02-14.
+//  Copyright (c) 2013 Evoco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FolderDTO : NSObject
+
+@end
