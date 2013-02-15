913934e00efb5751b7b82ec80af28034a937ad1d
Adam Clark <adamclark@Adams-MacBook-Pro.local>
2013-02-14 18:57:03 -0700

    use custom segue to navigate from login screen to SpitViewController

---
+++ Evoco Docs/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  fuze Docs
+//
+//  Created by Adam Clark on 2013-02-14.
+//  Copyright (c) 2013 Evoco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UISplitViewController
+
+@end
