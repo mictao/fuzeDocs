d6abc6217d45729d97ff1d872d1fc8ef66f31bc5
Michelle <mitchele.tao@gmail.com>
2013-02-18 13:00:48 -0700

    Allow display of login screen for upside down orientation

---
+++ Evoco Docs/NavigationViewController.m
@@ -0,0 +1,51 @@
+//
+//  NavigationViewController.m
+//  fuze Docs
+//
+//  Created by Administrator on 13-02-18.
+//  Copyright (c) 2013 Evoco. All rights reserved.
+//
+
+#import "NavigationViewController.h"
+
+@interface NavigationViewController ()
+
+@end
+
+@implementation NavigationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (BOOL)shouldAutorotate
+{
+    return YES;
+}
+
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    return UIInterfaceOrientationMaskAll;
+}
+
+
+@end
